ed9fab7c876838e67affd8fd32dafabd8f310261
gjy <1415827169@qq.com>
2026-01-08 04:52:24 +0800

    config: update Vite configuration.

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/display/', // GitHub 仓库名，部署时资源路径前缀
+  base: '/display-pages/', // GitHub 仓库名，部署时资源路径前缀
 })
